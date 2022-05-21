f389a80270c343db71be1b96925b59ace4189a85
Anton Vassel <ant-vassel@ya.ru>
2022-05-21 15:12:55 +0300

    working app

---
+++ ContactList/ViewControllers/PersonAdvancedViewController.swift
@@ -0,0 +1,51 @@
+//
+//  PersonAdvancedDetailsViewController.swift
+//  ContactList
+//
+//  Created by Anton Vassel on 21.05.2022.
+//
+
+import UIKit
+
+class PersonAdvancedViewController: UITableViewController {
+    
+    var personContact: [Person] = []
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        personContact.count
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        personContact[section].fullName
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        2
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "personID", for: indexPath)
+
+        let person = personContact[indexPath.section]
+        var content = cell.defaultContentConfiguration()
+        
+        switch indexPath.row {
+        case 0:
+            content.text = person.phoneNumber
+            content.image = UIImage(systemName: "phone")
+        default:
+            content.text = person.email
+            content.image = UIImage(systemName: "mail")
+        }
+        
+        cell.contentConfiguration = content
+
+        return cell
+    }
+}
